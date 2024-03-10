70c163c1096024a99d737d8356d4128f4eff6234
zeyadsharo <zeyadsharo85@gmail.com>
2024-03-10 19:33:38 +0300

    Refactor Sector model's $fillable array

--- app/Models/Sector.php
+++ app/Models/Sector.php
@@ -8,5 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Sector extends Model
 {
     use HasFactory;
-      protected $fillable = ['arabic_title', 'kurdish_title', 'description', 'display_order', 'display_state', 'icon', 'activation_state'];
+      protected $fillable = ['arabic_title', 
+      'kurdish_title', 
+      'description',
+       'display_order',
+        'display_state', 
+        'icon', 
+        'activation_state'];
 }
