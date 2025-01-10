db5f4766cf47caeaba8ba1d19d491220bef0cc83
zeyad_sharo <zeyadsharo85@gmail.com>
2025-01-10 14:22:47 +0300

    feat: Disable creation of additional categories and properties after initial creation

--- app/Filament/Admin/Resources/CategoryResource/Pages/CreateCategory.php
+++ app/Filament/Admin/Resources/CategoryResource/Pages/CreateCategory.php
@@ -9,4 +9,5 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateCategory extends CreateRecord
 {
     protected static string $resource = CategoryResource::class;
+    protected static bool $canCreateAnother = false;
 }
--- app/Filament/Admin/Resources/PropertyResource/Pages/CreateProperty.php
+++ app/Filament/Admin/Resources/PropertyResource/Pages/CreateProperty.php
@@ -9,4 +9,6 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateProperty extends CreateRecord
 {
     protected static string $resource = PropertyResource::class;
+    protected static bool $canCreateAnother = false;
+
 }
