47048ec73f4d6d70e406e6edb569b4a1f2723e82
zeyad_sharo <zeyadsharo85@gmail.com>
2025-01-20 12:43:34 +0300

    feat: Update submit button styling in CustomerResource form for improved UI consistency

--- app/Filament/Admin/Resources/CustomerResource.php
+++ app/Filament/Admin/Resources/CustomerResource.php
@@ -182,7 +182,7 @@ class CustomerResource extends Resource
                                 ->default(now()->addMonths(3))
                                 ->label(__('Customer.label.next_payment')),
                         ])
-                ])->submitAction(new HtmlString('<button type="submit">' . __('Customer.submit') . '</button>'))
+                ])->submitAction(new HtmlString('<button type="submit" class="filament-button filament-button-size-md inline-flex items-center justify-center py-2 gap-1 font-medium rounded-lg border transition-colors outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset min-h-[2.25rem] px-4 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700">' . __('Customer.submit') . '</button>'))
                     ->columnSpanFull(),
             ]);
     }
